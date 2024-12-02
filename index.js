f2e740c0f8ce1cd1cc44bc040953661f44318877
borisinparis <mnkymgaa@gmail>
2024-12-02 17:16:01 +0800

    variables zarslasan

---
+++ index.js
@@ -0,0 +1,24 @@
+// let --> variable name example let age = 15
+// var --> name = "boris"
+//const -->name = "boris"// 
+console.log("hello world from variables ");
+let country = " mongolia";
+const name = "boris";
+const lastname= " paris"
+let age = 15;
+const fullName = name + lastname
+age = 20 ;
+console.log(age);
+console.log(fullName + " nertei  " + age + " nastai " + country + " ulsad amidardagaa");
+console.log(typeof name);
+
+
+
+// 1. "Comments can make code readable" - нэг мөртэй коммент болгох
+// 2. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+// 3. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined, null гэх мэт
+// 4. Утга оноогоогүй хувьсагч зарлах
+// 5. Утга оноосон хувьсагч зарлах
+// 6. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+// 7. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+
